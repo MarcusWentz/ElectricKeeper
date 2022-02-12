63ccde9fae3f5100adcb6f2dd3ad433a5075e907
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-11 22:16:09 -0500

    Add files via upload

---
+++ servo_loop_piblaster.js
@@ -0,0 +1,19 @@
+//https://github.com/sarfata/pi-blaster
+//Build and install directly from source
+var piblaster = require('pi-blaster.js');
+const timeMilliSec = 2000;
+const pulseWidthMin = 0.00;
+const pulseWidthMax = 0.35;
+let pulseWidth = pulseWidthMax
+
+setInterval(() => {
+  piblaster.setPwm(17, pulseWidth); 
+  piblaster.setPwm(23, pulseWidth); 
+  piblaster.setPwm(27, pulseWidth); 
+  console.log(pulseWidth)
+  if (pulseWidth == pulseWidthMin) {
+      pulseWidth = pulseWidthMax
+  } else if (pulseWidth == pulseWidthMax) {
+    	pulseWidth = pulseWidthMin;
+  }
+}, timeMilliSec);
