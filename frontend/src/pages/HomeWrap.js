e4af8f6ffbf89bd995c6261d1cda517119362087
Johanna <adrina0002@gmail.com>
2022-02-12 19:11:52 +0100

    added my own email

--- frontend/src/pages/HomeWrap.js
+++ frontend/src/pages/HomeWrap.js
@@ -6,7 +6,7 @@ const HomeWrap = () => {
       <div class="row">
         <div class="col-5">
           <h1><br></br>
-            <b>ETH Electric, buy your electricity using smart contracts</b>
+            <b>ETH Electric, buy your electricity using smart contracts!</b>
           </h1>
         </div>
         <div class="col-6">
