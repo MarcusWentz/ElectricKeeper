7337abde6d9889cc8541497618b8ce4793237528
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-13 20:31:28 -0500

    Update and rename priceFeed.sol to OnePennyUSDinMaticPriceFeed.sol

--- contract/OnePennyUSDinMaticPriceFeed.sol
+++ contract/OnePennyUSDinMaticPriceFeed.sol
@@ -1,27 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.11;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract PriceConsumerV3 {
+contract OnePennyUSDinMatic {
 
     AggregatorV3Interface internal priceFeed;
     address Owner;
 
-    /**
-     * Network: Matic
-     * Aggregator: Matic/USD
-     * Address: 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
-     */
     constructor() {
         Owner = msg.sender;
         priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
     }
     
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice() public view returns (int) {
+    function getLatestPrice() public view returns (uint) {
         (
             uint80 roundID, 
             int price,
@@ -29,11 +21,11 @@ contract PriceConsumerV3 {
             uint timeStamp,
             uint80 answeredInRound
         ) = priceFeed.latestRoundData();
-        return (1 * 10 ** 18 / price) * 10 ** 9;
+        return uint( (1 * 10 ** 18 / price) * 10 ** 6);
     }
 
-    function payDoller() public payable {
-        require(msg.value == uint(getLatestPrice()) , "amounts not equal");
-        payable(address(Owner)).transfer(uint(getLatestPrice()));
+    function payPenny() public payable {
+        require(msg.value == getLatestPrice() , "MSG_VALUE_DOES_NOT_MATCH_PRICEFEED_CONVERSION.");
+        payable(Owner).transfer(address(this).balance);
     }
 }
