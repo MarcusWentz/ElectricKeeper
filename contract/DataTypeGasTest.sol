4b39068ed4d1dafefd0d5e2959cdc50f0bf452ad
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-14 16:55:14 -0500

    Update DataTypeGasTest.sol

--- contract/DataTypeGasTest.sol
+++ contract/DataTypeGasTest.sol
@@ -1,17 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
-contract ElectricEthereum { //141457 GAS
+contract TwoArrays { //186999 GAS
     uint[4] public VoltageStates;
     uint[4] public ExpirationTimeUNIX;
+    constructor() {
+        VoltageStates[0] = 1;
+        ExpirationTimeUNIX[0] = 1;
+    }
 }
 
-contract TEST1 { //129331 GAS
+contract OneStructOneArray { //175077 GAS
     struct values{ uint VoltageStates; uint ExpirationTimeUNIX; }
     values[4] public LED;
+    constructor() {
+        LED[0].VoltageStates = 1;
+        LED[0].ExpirationTimeUNIX = 1;
+    }
 }
 
-contract TEST2 { //128029 GAS
+contract OneMappingOneStruct { //173229 GAS 
     struct values{ uint VoltageStates; uint ExpirationTimeUNIX; }
     mapping(uint => values) public LED; 
+    constructor() {
+        LED[0].VoltageStates = 1;
+        LED[0].ExpirationTimeUNIX = 1;
+    }
 }
