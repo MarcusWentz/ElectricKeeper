446f25f8a6b1d33fb72da64c7e934bebcbf31b9c
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-14 17:13:23 -0500

    Update and rename contract/ElectricEthereumV5.sol to contract/gasTests/ElectricEthereumV5.sol

--- contract/gasTests/ElectricEthereumV5.sol
+++ contract/gasTests/ElectricEthereumV5.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
-contract ElectricEthereum {
+contract ElectricEthereum { //770344 GAS
 
     uint[4] public VoltageStates;
     uint[4] public ExpirationTimeUNIX;
