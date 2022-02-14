fe30453a8ee8e2da94474cb225e4012223f5f1a0
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-14 17:14:21 -0500

    Create ElectricEthereumV6.sol

---
+++ contract/ElectricEthereumV6.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity 0.8.11;
+
+contract ElectricEthereum { //739184 GAS
+
+    struct STATE{ uint Voltage; uint ExpirationTimeUNIX; }
+    mapping(uint => STATE) public LED; 
+    address public immutable Owner;
+    event VoltageChange (uint RedVoltage, uint BlueVoltage, uint YellowVoltage, uint GreenVoltage);
+
+    constructor() {
+        Owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == Owner, "ONLY_OWNER_WALLET_ADDRESS_HAS_ACCESS.");
+        _;
+    }
+
+    function BuyElectricityTimeOn(uint ledValue, uint minutesToHaveOn) public payable {
+        require(ledValue >= 0 && ledValue < 4, "LED_VALUES_ALLOWED_ARE_RED_0_BLUE_1_GREEN_2_YELLOW_3.");
+        require(msg.value == minutesToHaveOn && minutesToHaveOn > 0, "MSG.VALUE_MUST_BE_1_WEI_TIMES_MINUTES_AND_NOT_BE_ZERO.");
+        if(LED[ledValue].Voltage == 0) {
+            LED[ledValue].Voltage = 1;
+            LED[ledValue].ExpirationTimeUNIX = block.timestamp + (60*minutesToHaveOn); 
+        } else {
+            LED[ledValue].ExpirationTimeUNIX  += (60*minutesToHaveOn); 
+        }
+        emit VoltageChange(LED[0].Voltage,LED[1].Voltage,LED[2].Voltage,LED[3].Voltage);
+        payable(Owner).transfer(address(this).balance);
+    }
+
+    function OwnerManualTurnOffElectricity() public onlyOwner {
+        require( (block.timestamp > LED[0].ExpirationTimeUNIX && LED[0].Voltage == 1) || 
+                 (block.timestamp > LED[1].ExpirationTimeUNIX && LED[1].Voltage == 1) || 
+                 (block.timestamp > LED[2].ExpirationTimeUNIX && LED[2].Voltage == 1) || 
+                 (block.timestamp > LED[3].ExpirationTimeUNIX && LED[3].ExpirationTimeUNIX == 1)   , "NO_EXPIRATION_YET.");
+        if(block.timestamp > LED[0].ExpirationTimeUNIX && LED[0].Voltage == 1){
+            LED[0].Voltage  = 0;
+            LED[0].ExpirationTimeUNIX = 0;
+        }
+        if(block.timestamp > LED[1].ExpirationTimeUNIX && LED[1].Voltage == 1){
+            LED[1].Voltage  = 0;
+            LED[1].ExpirationTimeUNIX = 0;
+        }
+        if(block.timestamp > LED[2].ExpirationTimeUNIX && LED[2].Voltage == 1){
+            LED[2].Voltage  = 0;
+            LED[2].ExpirationTimeUNIX = 0;
+        }
+        if(block.timestamp > LED[3].ExpirationTimeUNIX && LED[3].ExpirationTimeUNIX == 1){
+            LED[3].Voltage  = 0;
+            LED[3].ExpirationTimeUNIX = 0;
+        }
+        emit VoltageChange(LED[0].Voltage,LED[1].Voltage,LED[2].Voltage,LED[3].Voltage);
+    }
+}
