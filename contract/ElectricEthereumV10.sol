0507db0d20c7d31b7452302e53044eea1898a3b4
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-15 13:36:12 -0500

    Update and rename ElectricEthereumV9.sol to ElectricEthereumV10.sol

--- contract/ElectricEthereumV10.sol
+++ contract/ElectricEthereumV10.sol
@@ -36,25 +36,13 @@ contract ElectricEthereum {
                  (block.timestamp > LED[1].ExpirationTimeUNIX && LED[1].Voltage == 1) || 
                  (block.timestamp > LED[2].ExpirationTimeUNIX && LED[2].Voltage == 1) || 
                  (block.timestamp > LED[3].ExpirationTimeUNIX && LED[3].Voltage == 1)   , "NO_EXPIRATION_YET.");
-        if(block.timestamp > LED[0].ExpirationTimeUNIX && LED[0].Voltage == 1){
-            LED[0].Voltage  = 0;
-            LED[0].ExpirationTimeUNIX = 0;
-            LED[0].LatestBuyer = 0x0000000000000000000000000000000000000000;
-        }
-        if(block.timestamp > LED[1].ExpirationTimeUNIX && LED[1].Voltage == 1){
-            LED[1].Voltage  = 0;
-            LED[1].ExpirationTimeUNIX = 0;
-            LED[1].LatestBuyer = 0x0000000000000000000000000000000000000000;
-        }
-        if(block.timestamp > LED[2].ExpirationTimeUNIX && LED[2].Voltage == 1){
-            LED[2].Voltage  = 0;
-            LED[2].ExpirationTimeUNIX = 0;
-            LED[2].LatestBuyer = 0x0000000000000000000000000000000000000000;
-        }
-        if(block.timestamp > LED[3].ExpirationTimeUNIX && LED[3].Voltage == 1){
-            LED[3].Voltage  = 0;
-            LED[3].ExpirationTimeUNIX = 0;
-            LED[2].LatestBuyer = 0x0000000000000000000000000000000000000000;
+        for(uint currentLED; currentLED < 4; currentLED++) {
+            if(block.timestamp > LED[currentLED].ExpirationTimeUNIX && LED[currentLED].Voltage == 1){
+                LED[currentLED].Voltage  = 0;
+                LED[currentLED].ExpirationTimeUNIX = 0;
+                LED[currentLED].LatestBuyer = 0x0000000000000000000000000000000000000000;
+             }
+
         }
         emit VoltageChange();
     }
