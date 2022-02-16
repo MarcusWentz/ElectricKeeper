7778c4b0f8cf89826b23cb7752078328ef82aaa2
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-16 17:05:08 -0500

    Update DEBUG_TEST_piblasterPWM_LED.js

--- scripts/DEBUG_TEST_piblasterPWM_LED.js
+++ scripts/DEBUG_TEST_piblasterPWM_LED.js
@@ -8,8 +8,12 @@ let pulseWidth = pulseWidthMax
 
 setInterval(() => {
   piblaster.setPwm(17, pulseWidth); 
+  piblaster.setPwm(18, pulseWidth);
+  piblaster.setPwm(21, pulseWidth);  
   piblaster.setPwm(22, pulseWidth); 
   piblaster.setPwm(23, pulseWidth); 
+  piblaster.setPwm(24, pulseWidth); 
+  piblaster.setPwm(25, pulseWidth); 
   piblaster.setPwm(27, pulseWidth); 
   console.log(pulseWidth)
   if (pulseWidth == pulseWidthMin) {
