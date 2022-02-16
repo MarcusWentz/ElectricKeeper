186604d15be7d649134772eb96397bf8fd1a1513
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-16 16:50:34 -0500

    Update HardwarePiblasterPolygonWSS.js

--- scripts/HardwarePiblasterPolygonWSS.js
+++ scripts/HardwarePiblasterPolygonWSS.js
@@ -1,9 +1,9 @@
 const Web3 = require('web3') //HIDE KEYS WITH "Linux Environment Variables" https://www.youtube.com/watch?v=himEMfYQJ1w "vim .bashrc" > "i" > update > "esc" > ":w" enter
 const rpcURL =  process.env.PolygonMumbaiTestnetSubscribeAlchemyWSS //Use WSS to get live event data instead of polling constantly,
 const web3 = new Web3(rpcURL)
-const contractAddress_JS = '0x01FBD75A3bF18bc72FB188c9C18030F93D0d4Bd7'
+const contractAddress_JS = '0xB92376659C471f10239099E28f657F69415aF5D2'
 const contractABI_JS =
-[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"VoltageChange","type":"event"},{"inputs":[{"internalType":"uint256","name":"ledValue","type":"uint256"},{"internalType":"uint256","name":"minutesToHaveOn","type":"uint256"}],"name":"BuyElectricityTimeOn","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"LED","outputs":[{"internalType":"uint256","name":"Voltage","type":"uint256"},{"internalType":"uint256","name":"ExpirationTimeUNIX","type":"uint256"},{"internalType":"address","name":"LatestBuyer","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"Owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"OwnerManualTurnOffElectricity","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"VoltageChange","type":"event"},{"inputs":[{"internalType":"uint256","name":"ledValue","type":"uint256"},{"internalType":"uint256","name":"minutesToHaveOn","type":"uint256"}],"name":"BuyElectricityTimeOn","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"LED","outputs":[{"internalType":"uint256","name":"Voltage","type":"uint256"},{"internalType":"uint256","name":"ExpirationTimeUNIX","type":"uint256"},{"internalType":"address","name":"LatestBuyer","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"Owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"TurnOffElectricity","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"expirationOccured","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
 
 //https://github.com/sarfata/pi-blaster
@@ -18,7 +18,7 @@ let objectLED = {"pin":   [17   ,27    ,23      ,22     ,24      ,25      ,26   
 
 function checkValueLatest() { //get() contract value,
 
-  for(let ledValue = 0; ledValue < 4; ledValue++ ) {
+  for(let ledValue = 0; ledValue < 8; ledValue++ ) {
     contractDefined_JS.methods.LED(ledValue).call((err, balance) => {
       if(balance.Voltage == 1){
         console.log("COLOR " + objectLED['color'][ledValue] + " PIN " + objectLED['pin'][ledValue] + " ON!" )
