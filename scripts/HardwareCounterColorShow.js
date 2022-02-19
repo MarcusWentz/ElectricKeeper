a55764b40f8f4407a7534c520b60dcb43f15d037
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-19 15:11:39 -0500

    Update and rename HardwareCounterColorShow.js to DEBUGHardwareCounterColorShow.js

--- scripts/HardwareCounterColorShow.js
+++
@@ -1,7 +0,0 @@
-for(let i = 0; i < 256 ; i++) {
-  console.log("VALUE: " + i)
-  for(let j = 1; j<9 ; j++) {
-    console.log("BIT " + j + ":" )
-    console.log((i&(2**(j-1))) == (2**(j-1)) )
-  }
-}
