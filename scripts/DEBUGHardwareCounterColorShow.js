3466c6dfb5f71043804d953a8014217788da0307
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-19 16:46:45 -0500

    Update DEBUGHardwareCounterColorShow.js

--- scripts/DEBUGHardwareCounterColorShow.js
+++ scripts/DEBUGHardwareCounterColorShow.js
@@ -1,12 +1,12 @@
-// var piblaster = require('pi-blaster.js');
-const timeMilliSec = 1000; //1000 is one second
+var piblaster = require('pi-blaster.js');
+const timeMilliSec = 100; //1000 is one second
 const pulseWidthMin = 0.00;
 const pulseWidthMax = 0.35;
 
 let objectLED = {"pin":   [17   ,27    ,23      ,22     ,24      ,25      ,18    ,21     ],
                  "color": ['RED','BLUE','YELLOW','GREEN','PURPLE','ORANGE','GREY','WHITE']}
 
-function checkValueLatest() { //get() contract value,
+function count() { //get() contract value,
     for(let i = 0; i < 256 ; i++) {
       setTimeout(() => {bitCheck(i)}, i*timeMilliSec);
       }
@@ -17,12 +17,12 @@ function bitCheck(i) {
   for(let j = 0; j<8 ; j++) {
     if(  (i&(2**(j))) == (2**(j)) ){
       console.log("COLOR " + objectLED['color'][j] + " PIN " + objectLED['pin'][j] + " ON!" )
-      // piblaster.setPwm(objectLED['pin'][ledValue], pulseWidthMax);
+      piblaster.setPwm(objectLED['pin'][j], pulseWidthMax);
     } else {
       console.log("COLOR " + objectLED['color'][j]  + " PIN " +  objectLED['pin'][j] + " OFF!" )
-      // piblaster.setPwm(objectLED['pin'][j], pulseWidthMin);
+      piblaster.setPwm(objectLED['pin'][j], pulseWidthMin);
     }
    }
 }
 
-checkValueLatest()
+count()
