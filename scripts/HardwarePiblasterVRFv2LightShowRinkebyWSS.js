b913234c1785bcc4477c68104d613cb7fb683eb2
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-22 08:47:51 -0500

    Update HardwarePiblasterVRFv2LightShowRinkebyWSS.js

--- scripts/HardwarePiblasterVRFv2LightShowRinkebyWSS.js
+++ scripts/HardwarePiblasterVRFv2LightShowRinkebyWSS.js
@@ -1,10 +1,9 @@
 const Web3 = require('web3') //HIDE KEYS WITH "Linux Environment Variables" https://www.youtube.com/watch?v=himEMfYQJ1w "vim .bashrc" > "i" > update > "esc" > ":w" enter
 const rpcURL =  process.env.rinkebyWebSocketSecureEventsInfuraAPIKey //Use WSS to get live event data instead of polling constantly,
 const web3 = new Web3(rpcURL)
-const contractAddress_JS = '0xcCd74CAc275693e8BFB877Ac937C5320225AB27b'
+const contractAddress_JS = '0xD111A5E51034A17505f82547Ad3508EbCFc7c405'
 const contractABI_JS =
-[{"inputs":[{"internalType":"uint64","name":"subscriptionId","type":"uint64"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"have","type":"address"},{"internalType":"address","name":"want","type":"address"}],"name":"OnlyCoordinatorCanFulfill","type":"error"},{"anonymous":false,"inputs":[],"name":"lightShowUpdate","type":"event"},{"inputs":[{"internalType":"uint256","name":"requestId","type":"uint256"},{"internalType":"uint256[]","name":"randomWords","type":"uint256[]"}],"name":"rawFulfillRandomWords","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"requestRandomWords","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"s_randomWords","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"s_requestId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
-
+[{"inputs":[{"internalType":"address","name":"have","type":"address"},{"internalType":"address","name":"want","type":"address"}],"name":"OnlyCoordinatorCanFulfill","type":"error"},{"anonymous":false,"inputs":[],"name":"lightShowUpdate","type":"event"},{"inputs":[{"internalType":"uint256","name":"requestId","type":"uint256"},{"internalType":"uint256[]","name":"randomWords","type":"uint256[]"}],"name":"rawFulfillRandomWords","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"requestRandomWords","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"twoRandomWords","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
 const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
 
 //https://github.com/sarfata/pi-blaster
@@ -24,8 +23,8 @@ function timeout(ms) {
 async function updateLights() {
  while(true){
   for(let randomNumbers = 0; randomNumbers < 2; randomNumbers++ ) {
-    contractDefined_JS.methods.s_randomWords(randomNumbers).call((err, balance) => {
-      console.log(balance)
+    contractDefined_JS.methods.twoRandomWords(randomNumbers).call((err, balance) => {
+      console.log(balance%255)
       for(let i = 0 ; i < 1 ; i++) {
         for(let ledValue = 0; ledValue < 8; ledValue++ ) {
             if(  (balance&(2**(ledValue))) == (2**(ledValue)) ){
