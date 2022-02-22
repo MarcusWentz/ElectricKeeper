b279f0b0c25794a3059748a1d6ab7f6152cdbf44
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-21 23:09:18 -0500

    Update and rename ElectricEthereumV17.sol to ElectricEthereumV19.sol

--- contract/ElectricEthereumV19.sol
+++ contract/ElectricEthereumV19.sol
@@ -20,7 +20,7 @@ contract ElectricEthereum is KeeperCompatibleInterface {
     }
 
     function BuyElectricityTimeOn(uint ledValue, uint minutesToHaveOn) public payable {
-        require(ledValue >= 0 && ledValue < 8, "LED_VALUES_RED_0_BLUE_1_YELLOW_2_GREEN_3_PURPLE_4_ORANGE_5_GREY_6_WHITE_7.");
+        require(ledValue >= 0 && ledValue < 8, "LED_VALUES_RED_0_BLUE_1_YELLOW_2_GREEN_3_PURPLE_4_ORANGE_5_PINK_6_WHITE_7.");
         require(msg.value == minutesToHaveOn && minutesToHaveOn > 0, "MSG.VALUE_MUST_BE_1_WEI_TIMES_MINUTES_AND_NOT_BE_ZERO.");
         if(LED[ledValue].Voltage == 0) {
             LED[ledValue].Voltage = 1;
@@ -33,8 +33,11 @@ contract ElectricEthereum is KeeperCompatibleInterface {
         payable(Owner).transfer(address(this).balance);
     }
 
-    function TurnOffElectricity() public onlyOwner {
-        require(expirationOccured() , "NO_EXPIRATION_YET.");
+    function checkUpkeep(bytes calldata) external override returns (bool upkeepNeeded, bytes memory) {
+        upkeepNeeded = expirationOccured();
+    } 
+
+    function performUpkeep(bytes calldata) external override {
         for(uint ledValue = 0; ledValue < 8; ledValue++) {
             if(LED[ledValue].Voltage == 1 && block.timestamp > LED[ledValue].ExpirationTimeUNIX){
                 LED[ledValue].Voltage  = 0;
@@ -54,11 +57,8 @@ contract ElectricEthereum is KeeperCompatibleInterface {
         return false;
     }
 
-    function checkUpkeep(bytes calldata) external override returns (bool upkeepNeeded, bytes memory) {
-        upkeepNeeded = expirationOccured();
-    } 
-
-    function performUpkeep(bytes calldata) external override {
+    function OwnerManualExpirationOff() public onlyOwner {
+        require(expirationOccured() , "NO_EXPIRATION_YET.");
         for(uint ledValue = 0; ledValue < 8; ledValue++) {
             if(LED[ledValue].Voltage == 1 && block.timestamp > LED[ledValue].ExpirationTimeUNIX){
                 LED[ledValue].Voltage  = 0;
@@ -66,6 +66,18 @@ contract ElectricEthereum is KeeperCompatibleInterface {
                 LED[ledValue].LatestBuyer = 0x0000000000000000000000000000000000000000;
             }
         }
+        emit VoltageChange();
+    }
+
+    function OwnerEmergencyOff(uint _ledValue) public onlyOwner {
+        LED[_ledValue].Voltage  = 2;
+        LED[_ledValue].ExpirationTimeUNIX -= block.timestamp;
+        emit VoltageChange();
+    }
+
+    function OwnerRestoreOn(uint _ledValue) public onlyOwner {
+        LED[_ledValue].Voltage  = 1;
+        LED[_ledValue].ExpirationTimeUNIX += block.timestamp;
         emit VoltageChange();
     }
 
