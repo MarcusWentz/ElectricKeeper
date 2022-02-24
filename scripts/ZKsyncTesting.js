e3b6fb26f9813888ff2f25d6b651b98a60ed4631
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-24 12:31:14 -0500

    Update ZKsyncTesting.js

--- scripts/ZKsyncTesting.js
+++ scripts/ZKsyncTesting.js
@@ -2,13 +2,23 @@ const { Wallet } = require('zksync');
 const { ethers } = require('ethers');
 let zksync = require('zksync')
 
-async function connectZKsyncRinkeby(){
+async function depositRinkebyETHtoZKsync() {
   const syncProvider = await zksync.getDefaultProvider('rinkeby');
   console.log(syncProvider)
   const ethersProvider = ethers.getDefaultProvider('rinkeby');
   console.log(ethersProvider)
-  const signer = new ethers.Wallet(process.env.devTestnetPrivateKey, ethersProvider);
-  console.log(signer)
+  const ethWallet = new ethers.Wallet(process.env.devTestnetPrivateKey, ethersProvider);
+  console.log(ethWallet)
+  const syncWallet = await zksync.Wallet.fromEthSigner(ethWallet, syncProvider);
+  console.log(syncWallet)
+  //Deposit
+  const deposit = await syncWallet.depositToSyncFromEthereum({
+    depositTo: syncWallet.address(),
+    token: 'ETH',
+    amount: ethers.utils.parseEther('0.1')
+  });
+  const depositReceipt = await deposit.awaitReceipt();
+  const depositReceiptVerify = await deposit.awaitVerifyReceipt();
 }
 
-connectZKsyncRinkeby()
+depositRinkebyETHtoZKsync()
