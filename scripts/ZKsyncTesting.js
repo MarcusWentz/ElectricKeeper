dd5d21bfa45557fb7446abf005ad829c9daf292c
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-24 12:22:02 -0500

    Update ZKsyncTesting.js

--- scripts/ZKsyncTesting.js
+++ scripts/ZKsyncTesting.js
@@ -1,8 +1,14 @@
+const { Wallet } = require('zksync');
+const { ethers } = require('ethers');
 let zksync = require('zksync')
 
 async function connectZKsyncRinkeby(){
   const syncProvider = await zksync.getDefaultProvider('rinkeby');
   console.log(syncProvider)
+  const ethersProvider = ethers.getDefaultProvider('rinkeby');
+  console.log(ethersProvider)
+  const signer = new ethers.Wallet(process.env.devTestnetPrivateKey, ethersProvider);
+  console.log(signer)
 }
 
 connectZKsyncRinkeby()
