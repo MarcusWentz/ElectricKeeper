8ea50e67cc911d5c241e03cc00bb1325d8c0f5fb
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-24 12:59:32 -0500

    Update ZKsyncTesting.js

--- scripts/ZKsyncTesting.js
+++ scripts/ZKsyncTesting.js
@@ -4,21 +4,48 @@ let zksync = require('zksync')
 
 async function depositRinkebyETHtoZKsync() {
   const syncProvider = await zksync.getDefaultProvider('rinkeby');
-  console.log(syncProvider)
+  // console.log(syncProvider)
   const ethersProvider = ethers.getDefaultProvider('rinkeby');
-  console.log(ethersProvider)
+  // console.log(ethersProvider)
   const ethWallet = new ethers.Wallet(process.env.devTestnetPrivateKey, ethersProvider);
-  console.log(ethWallet)
+  // console.log(ethWallet)
   const syncWallet = await zksync.Wallet.fromEthSigner(ethWallet, syncProvider);
-  console.log(syncWallet)
+  // console.log(syncWallet)
+
   //Deposit
   const deposit = await syncWallet.depositToSyncFromEthereum({
     depositTo: syncWallet.address(),
     token: 'ETH',
-    amount: ethers.utils.parseEther('0.1')
+    amount: ethers.utils.parseEther('0.200')
   });
   const depositReceipt = await deposit.awaitReceipt();
   const depositReceiptVerify = await deposit.awaitVerifyReceipt();
+
+  //NEED TO UNLOCK ACCOUNT FIRST
+  if (!(await syncWallet.isSigningKeySet())) {
+    if ((await syncWallet.getAccountId()) == undefined) {
+      throw new Error('Unknown account');
+    }
+  const changePubkey = await syncWallet.setSigningKey({
+    feeToken: 'ETH',
+    ethAuthType: 'ECDSA'
+  });
+  await changePubkey.awaitReceipt();
+}
+
+  const committedETHBalance = await syncWallet.getBalance('ETH');
+  console.log(committedETHBalance)
+
+  const withdraw = await syncWallet.withdrawFromSyncToEthereum({
+    ethAddress: ethWallet.address,
+    token: 'ETH',
+    amount: ethers.utils.parseEther('0.198')
+  });
+
+  await withdraw.awaitVerifyReceipt();
+
+  // const committedETHBalance = await syncWallet.getBalance('ETH');
+  // console.log(committedETHBalance)
 }
 
 depositRinkebyETHtoZKsync()
