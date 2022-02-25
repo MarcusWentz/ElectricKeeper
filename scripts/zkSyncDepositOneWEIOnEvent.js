5b1f51869a25ba3a4012d1fdbb597277768d02ec
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-02-24 20:16:12 -0500

    Update and rename zkSyncDepositOneWEI.js to zkSyncDepositOneWEIOnEvent.js

---
+++ scripts/zkSyncDepositOneWEIOnEvent.js
@@ -0,0 +1,89 @@
+const Web3 = require('web3') //HIDE KEYS WITH "Linux Environment Variables" https://www.youtube.com/watch?v=himEMfYQJ1w "vim .bashrc" > "i" > update > "esc" > ":w" enter
+const rpcURL =  process.env.PolygonMumbaiTestnetSubscribeAlchemyWSS //Use WSS to get live event data instead of polling constantly,
+const web3 = new Web3(rpcURL)
+const contractAddress_JS = '0x097C8609a052E99746D6C836f459dBbbA2DFB52F'
+const contractABI_JS =
+[{"anonymous":false,"inputs":[],"name":"VoltageChange","type":"event"},{"inputs":[{"internalType":"uint256","name":"ledValue","type":"uint256"},{"internalType":"uint256","name":"minutesToHaveOn","type":"uint256"}],"name":"BuyElectricityTimeOn","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"bytes","name":"","type":"bytes"}],"name":"checkUpkeep","outputs":[{"internalType":"bool","name":"upkeepNeeded","type":"bool"},{"internalType":"bytes","name":"","type":"bytes"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes","name":"","type":"bytes"}],"name":"performUpkeep","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"TurnOffElectricity","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"expirationOccured","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"LED","outputs":[{"internalType":"uint256","name":"Voltage","type":"uint256"},{"internalType":"uint256","name":"ExpirationTimeUNIX","type":"uint256"},{"internalType":"address","name":"LatestBuyer","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"Owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
+const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
+
+//
+//
+// const Web3 = require('web3') //HIDE KEYS WITH "Linux Environment Variables" https://www.youtube.com/watch?v=himEMfYQJ1w "vim .bashrc" > "i" > update > "esc" > ":w" enter
+// const rpcURL =  process.env.rinkebyWebSocketSecureEventsInfuraAPIKey //Use WSS to get live event data instead of polling constantly,
+// const web3 = new Web3(rpcURL)
+// const contractAddress_JS = '0xD111A5E51034A17505f82547Ad3508EbCFc7c405'
+// const contractABI_JS =
+// [{"inputs":[{"internalType":"address","name":"have","type":"address"},{"internalType":"address","name":"want","type":"address"}],"name":"OnlyCoordinatorCanFulfill","type":"error"},{"anonymous":false,"inputs":[],"name":"lightShowUpdate","type":"event"},{"inputs":[{"internalType":"uint256","name":"requestId","type":"uint256"},{"internalType":"uint256[]","name":"randomWords","type":"uint256[]"}],"name":"rawFulfillRandomWords","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"requestRandomWords","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"twoRandomWords","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+// const contractDefined_JS = new web3.eth.Contract(contractABI_JS, contractAddress_JS)
+
+const { Wallet } = require('zksync');
+const { ethers } = require('ethers');
+let zksync = require('zksync')
+
+var player = require('play-sound')(opts = {})
+
+async function depositOneWEIRinkebyzkSync() {
+  player.play('VoltageAlertDetected.mp3', function(err){if (err) throw err})
+  const syncProvider = await zksync.getDefaultProvider('rinkeby');
+  const ethersProvider = ethers.getDefaultProvider('rinkeby');
+  const ethWallet = new ethers.Wallet(process.env.devTestnetPrivateKey, ethersProvider);
+  const syncWallet = await zksync.Wallet.fromEthSigner(ethWallet, syncProvider);
+
+  const deposit = await syncWallet.depositToSyncFromEthereum({
+    depositTo: syncWallet.address(),
+    token: 'ETH',
+    amount: 1 //1 WEI
+  });
+  const depositReceipt = await deposit.awaitReceipt();
+  const depositReceiptVerify = await deposit.awaitVerifyReceipt();
+
+  if (!(await syncWallet.isSigningKeySet())) {
+    if ((await syncWallet.getAccountId()) == undefined) {
+      throw new Error('Unknown account');
+    }
+  const changePubkey = await syncWallet.setSigningKey({
+    feeToken: 'ETH',
+    ethAuthType: 'ECDSA'
+  });
+  await changePubkey.awaitReceipt();
+}
+
+  const committedETHBalance1 = await syncWallet.getBalance('ETH');
+  console.log(committedETHBalance1)
+
+  // const withdraw = await syncWallet.withdrawFromSyncToEthereum({
+  //   ethAddress: ethWallet.address,
+  //   token: 'ETH',
+  //   amount: ethers.utils.parseWei('1')
+  // });
+  //
+  // await withdraw.awaitVerifyReceipt();
+  //
+  // const committedETHBalance2 = await syncWallet.getBalance('ETH');
+  // console.log(committedETHBalance2)
+}
+
+// contractDefined_JS.events.lightShowUpdate({ //Subscribe to event.
+//      fromBlock: 'latest'
+//  }, function(error, eventResult){})
+//  .on('data', function(eventResult){
+//    console.log("EVENT DETECTED! NEW STATE VALUE: ")
+//    depositOneWEIRinkebyzkSync()
+//    })
+//  .on('changed', function(eventResult){
+//      // remove event from local database
+//  })
+//  .on('error', console.error);
+
+
+ contractDefined_JS.events.VoltageChange({ //Subscribe to event.
+      fromBlock: 'latest'
+  }, function(error, eventResult){})
+  .on('data', function(eventResult){
+    console.log("EVENT DETECTED! NEW STATE VALUE: ")
+    depositOneWEIRinkebyzkSync()
+    })
+  .on('changed', function(eventResult){
+      // remove event from local database
+  })
+  .on('error', console.error);
