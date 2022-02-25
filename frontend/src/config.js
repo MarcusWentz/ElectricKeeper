4e05af1a3b69931c3c3a07a3a61d945ae00e8f82
Johanna <adrina0002@gmail.com>
2022-02-25 18:39:18 +0100

    updated electricKeeper ABI and contract address

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,3 @@
-
-
 export const CONTRACT_ADDRESS = "0x3231A32ed0B2234Ebe88A0b1476B2b00c75BA144";
 export const ABI = [
   {
@@ -29,135 +27,82 @@ export const ABI = [
   },
 ];
 
-
-export const ELECTRICKEEPER_CONTRACT_ADDRESS = "0x097C8609a052E99746D6C836f459dBbbA2DFB52F"
+export const ELECTRICKEEPER_CONTRACT_ADDRESS =
+  "0x4d2f20f726Fe14e41b73B4439E399c82F4fB1775";
 export const ELECTRICKEEPER_ABI = [
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [],
-		"name": "VoltageChange",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "ledValue",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "minutesToHaveOn",
-				"type": "uint256"
-			}
-		],
-		"name": "BuyElectricityTimeOn",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "LED",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "Voltage",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "ExpirationTimeUNIX",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "LatestBuyer",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "Owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "TurnOffElectricity",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "bytes",
-				"name": "",
-				"type": "bytes"
-			}
-		],
-		"name": "checkUpkeep",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "upkeepNeeded",
-				"type": "bool"
-			},
-			{
-				"internalType": "bytes",
-				"name": "",
-				"type": "bytes"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "expirationOccured",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "bytes",
-				"name": "",
-				"type": "bytes"
-			}
-		],
-		"name": "performUpkeep",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
+  { anonymous: false, inputs: [], name: "VoltageChange", type: "event" },
+  {
+    inputs: [
+      { internalType: "uint256", name: "ledValue", type: "uint256" },
+      { internalType: "uint256", name: "minutesToHaveOn", type: "uint256" },
+    ],
+    name: "BuyElectricityTimeOn",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "bytes", name: "", type: "bytes" }],
+    name: "checkUpkeep",
+    outputs: [
+      { internalType: "bool", name: "upkeepNeeded", type: "bool" },
+      { internalType: "bytes", name: "", type: "bytes" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "ledValue", type: "uint256" }],
+    name: "OwnerEmergencyDangerOff",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "ledValue", type: "uint256" }],
+    name: "OwnerEmergencySafeOn",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "OwnerManualExpirationOff",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "bytes", name: "", type: "bytes" }],
+    name: "performUpkeep",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+  {
+    inputs: [],
+    name: "expirationOccured",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "LED",
+    outputs: [
+      { internalType: "uint256", name: "Voltage", type: "uint256" },
+      { internalType: "uint256", name: "ExpirationTimeUNIX", type: "uint256" },
+      { internalType: "address", name: "LatestBuyer", type: "address" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "Owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+];
