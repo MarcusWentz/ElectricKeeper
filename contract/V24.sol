02421c10a8143b57546662e33cc26c8dd2efd013
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-03-11 08:57:23 -0500

    Update V24.sol

--- contract/V24.sol
+++ contract/V24.sol
@@ -106,7 +106,7 @@ contract BuyDemoEightMinutes {
     function BuyTestEightMinuteCountdown() public payable {
        require(msg.value == electricKeeperInstance.onePennyUSDinMatic(36), "MUST_HAVE_MSG_VALUE=36*FEE.");
        for(uint ledValue = 0; ledValue < 8; ledValue++ ) {
-            electricKeeperInstance.BuyElectricityTimeOn{value: (ledValue+1)*electricKeeperInstance.onePennyUSDinMatic(ledValue+1)}(ledValue,ledValue+1);
+            electricKeeperInstance.BuyElectricityTimeOn{value: electricKeeperInstance.onePennyUSDinMatic(ledValue+1)}(ledValue,ledValue+1);
         }
     }
 }
