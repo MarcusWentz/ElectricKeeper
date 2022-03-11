348d60a76786d2ae5e0ff9f2cc94b7dd0dea7a18
Johanna <adrina0002@gmail.com>
2022-03-11 13:47:42 +0100

    styling of table

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -2,39 +2,62 @@ import EthLogo from "../assets/svg/eth_logo.svg";
 
 const Status = () => {
   return (
-    <div class="row" style={{width: '50%'}}>
-    <div class="center">
-    <table class="table table-hover" style={{background: 'white'}}>
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
-    </div>    </div>
-
+    <div class="row" style={{ width: "70%",  marginLeft: 'auto',marginRight: 'auto', marginTop: 50
+ }}>
+      <div class="center">
+        <table class="table table-hover btn-hover color-electric" style={{  borderRadius: 30, border: 'none' }}>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Voltage</th>
+              <th scope="col">Expiration</th>
+            </tr>
+          </thead>
+          <tbody style={{ background: "white", borderRadius: 30, border: 'none' }}>
+            <tr className="btn-hover color-red">
+              <th  scope="row">1</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+            <tr className="btn-hover color-blue">
+              <th scope="row">2</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+            <tr className="btn-hover color-yellow">
+              <th scope="row">3</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+            <tr className="btn-hover color-green">
+              <th scope="row">4</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+            <tr className="btn-hover color-purple">
+              <th scope="row">5</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+            <tr className="btn-hover color-orange">
+              <th scope="row">6</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+            <tr className="btn-hover color-pink">
+              <th scope="row">7</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>{" "}
+            <tr className="btn-hover color-white">
+              <th scope="row">8</th>
+              <td>x</td>
+              <td>y</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>{" "}
+    </div>
   );
 };
 
