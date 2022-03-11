16ceff90c206da23c042650ea6325efc2d404e11
Johanna <adrina0002@gmail.com>
2022-03-11 13:58:40 +0100

    center table

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -2,10 +2,20 @@ import EthLogo from "../assets/svg/eth_logo.svg";
 
 const Status = () => {
   return (
-    <div class="row" style={{ width: "70%",  marginLeft: 'auto',marginRight: 'auto', marginTop: 50
- }}>
+    <div
+      class="row"
+      style={{
+        width: "70%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        paddingTop: "100px",
+      }}
+    >
       <div class="center">
-        <table class="table table-hover btn-hover color-electric" style={{  borderRadius: 30, border: 'none' }}>
+        <table
+          class="table table-hover btn-hover color-electric"
+          style={{ background: "#fddd9e", borderRadius: 30, border: "none" }}
+        >
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -13,9 +23,11 @@ const Status = () => {
               <th scope="col">Expiration</th>
             </tr>
           </thead>
-          <tbody style={{ background: "white", borderRadius: 30, border: 'none' }}>
+          <tbody
+            style={{ background: "white", borderRadius: 30, border: "none" }}
+          >
             <tr className="btn-hover color-red">
-              <th  scope="row">1</th>
+              <th scope="row">1</th>
               <td>x</td>
               <td>y</td>
             </tr>
