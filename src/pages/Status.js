ae52d6ba5b311fba5de95cd8b87e4ad00a97d2c8
Johanna <adrina0002@gmail.com>
2022-03-11 13:25:50 +0100

    added table on status page

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -2,21 +2,39 @@ import EthLogo from "../assets/svg/eth_logo.svg";
 
 const Status = () => {
   return (
-    <div class="container">
-    <div class="row">
-      <div class="col-5">
-        <h1><br></br>
-          <b>About</b>
-        </h1>
-        <p>Submission for the ETH Denver Virtual Hackathon #BUIDL event</p>
-          <br></br>
-       </div>
-      <div class="col-6">
-        <img src={EthLogo} style={{ width: "40%" }} />
-      </div>
-      <div class="col"></div>
-    </div>
-  </div>
+    <div class="row" style={{width: '50%'}}>
+    <div class="center">
+    <table class="table table-hover" style={{background: 'white'}}>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>    </div>
+
   );
 };
 
