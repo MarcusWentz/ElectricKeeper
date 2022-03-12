fdfab9ed4a6cc6c3ed0c8390b5f58f056668e359
Kieran Williams <kieranwilliams@Kierans-MacBook-Air.local>
2022-03-12 16:15:43 +0000

    fixed firefox scale

---
+++ src/pages/Status.js
@@ -0,0 +1,29 @@
+import EthLogo from "../assets/svg/eth_logo.svg";
+
+const Status = () => {
+  return (
+    <div class="container" style={{ marginTop: "-70px" }}>
+      <div class="row">
+        <div>
+          <h1>
+            <b>Mobile Detected :(</b>
+          </h1>
+          <p>
+            Unfortunately we are not currently mobile compatible. Feel free to
+            check us out on a desktop!
+          </p>
+        </div>
+        <div class="row">
+          <div>
+            <br />
+            <img src={EthLogo} style={{ height: "20vh" }} />
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Status;
