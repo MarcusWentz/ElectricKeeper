3968fc3621e23447be0ed417733eeecd64cbc341
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-03-13 16:44:40 -0400

    Update polygonAPIworkingexample.sol

--- contract/polygonAPIworkingexample.sol
+++ contract/polygonAPIworkingexample.sol
@@ -1,10 +1,11 @@
-pragma solidity ^0.6.0;
+pragma solidity 0.8.12;
 
-import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
 contract APIConsumer is ChainlinkClient {
+        using Chainlink for Chainlink.Request; // This is the new line
   
-    uint256 public price;
+    uint256 public ElectricRateTennessee;
     
     address private oracle;
     bytes32 private jobId;
@@ -17,7 +18,7 @@ contract APIConsumer is ChainlinkClient {
      * LINK address: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
      * Fee: 0.01 LINK
      */
-    constructor() public {
+    constructor() {
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         oracle = 0xc8D925525CA8759812d0c299B90247917d4d4b7C;
         jobId = "bbf0badad29d49dc887504bacfbb905b";
@@ -28,45 +29,17 @@ contract APIConsumer is ChainlinkClient {
      * Create a Chainlink request to retrieve API response, find the target price
      * data, then multiply by 100 (to remove decimal places from price).
      */
-    function requestBTCCNYPrice() public returns (bytes32 requestId) 
-    {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        
-        // Set the URL to perform the GET request on
-        // NOTE: If this oracle gets more than 5 requests from this job at a time, it will not return. 
-        request.add("get", "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=CNY&apikey=demo");
-        
-       // Set the path to find the desired data in the API response, where the response format is:
-       // {
-       //     "Realtime Currency Exchange Rate": {
-       //       "1. From_Currency Code": "BTC",
-       //       "2. From_Currency Name": "Bitcoin",
-       //       "3. To_Currency Code": "CNY",
-       //       "4. To_Currency Name": "Chinese Yuan",
-       //       "5. Exchange Rate": "207838.88814500",
-       //       "6. Last Refreshed": "2021-01-26 11:11:07",
-       //       "7. Time Zone": "UTC",
-       //      "8. Bid Price": "207838.82343000",
-       //       "9. Ask Price": "207838.88814500"
-       //     }
-       //     }
-        string[] memory path = new string[](2);
-        path[0] = "Realtime Currency Exchange Rate";
-        path[1] = "5. Exchange Rate";
-        request.addStringArray("path", path);
-        
-        // Multiply the result by 10000000000 to remove decimals
-        request.addInt("times", 10000000000);
-        
-        // Sends the request
-        return sendChainlinkRequestTo(oracle, request, fee);
+    function requestElectricRateTennessee() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest("bbf0badad29d49dc887504bacfbb905b", address(this), this.fulfill.selector); //UINT
+        request.add("get", "https://developer.nrel.gov/api/utility_rates/v3.json?api_key=DEMO_KEY&lat=35&lon=-85");
+        request.add("path", "outputs.residential");
+        int timesAmount = 10000;
+        request.addInt("times", timesAmount);
+        return sendChainlinkRequestTo(0xc8D925525CA8759812d0c299B90247917d4d4b7C, request, 10**16); //0.01 LINK
     }
     
-    /**
-     * Receive the response in the form of uint256
-     */ 
-    function fulfill(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId)
-    {
-        price = _price;
+    function fulfill(bytes32 _requestId, uint256 _electricRateTennessee) public recordChainlinkFulfillment(_requestId) {
+        ElectricRateTennessee = _electricRateTennessee;
     }
+
 }
