77c80d2e7d46e48dab872cfae79748b6339b739a
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-03-12 23:38:27 -0500

    Update electricRateLocationAPI_Kovan.sol

--- contract/experiment/electricRateLocationAPI_Kovan.sol
+++ contract/experiment/electricRateLocationAPI_Kovan.sol
@@ -6,33 +6,25 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 contract APIConsumer is ChainlinkClient {
     using Chainlink for Chainlink.Request;
   
-    uint256 public volume;
+    uint256 public electricRateTennessee; //Resolution is $0.0000
     
-    address private oracle;
-    bytes32 private jobId;
-    uint256 private fee;
-    
-     
     constructor() {
         setPublicChainlinkToken();
-       oracle = 0xF405B99ACa8578B9eb989ee2b69D518aaDb90c1F;
-       jobId = "c51694e71fa94217b0f4a71b2a6b565a";
-        fee = 1 * 10 ** 16; // (Varies by network and job)
     }
 
     function requestVolumeData() public returns (bytes32 requestId) 
     {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        Chainlink.Request memory request = buildChainlinkRequest("c51694e71fa94217b0f4a71b2a6b565a", address(this), this.fulfill.selector);
         request.add("get", "https://developer.nrel.gov/api/utility_rates/v3.json?api_key=DEMO_KEY&lat=35&lon=-85");
         request.add("path", "outputs.residential");
-        int timesAmount = 10**18;
+        int timesAmount = 10000;
         request.addInt("times", timesAmount);
-        return sendChainlinkRequestTo(oracle, request, fee);
+        return sendChainlinkRequestTo(0xF405B99ACa8578B9eb989ee2b69D518aaDb90c1F, request, 10**16);
     }
     
-    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId)
+    function fulfill(bytes32 _requestId, uint256 _electricRateTennessee) public recordChainlinkFulfillment(_requestId)
     {
-        volume = _volume;
+        electricRateTennessee = _electricRateTennessee;
     }
 
 }
